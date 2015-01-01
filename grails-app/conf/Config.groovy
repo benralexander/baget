d10b474095be0f68d68d2f316faf031ba671ba91
Ben Alexander <balexand@broadinstitute.org>
2014-12-31 20:38:11 -0500

    I changed the config how?

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -92,7 +92,7 @@ environments {
     production {
         grails.logging.jul.usebridge = false
         // TODO: grails.serverURL = "http://www.changeme.com"
-        grails.serverURL = "http://bagetsoftware.org"
+        grails.serverURL = "http://bagetprod.elasticbeanstalk.com"
     }
 }
 
